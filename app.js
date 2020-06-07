37afb20be3243ffb407e053dd88dc78402e7b76a
Shayan6996 <fuhrer6996@gmail.com>
2020-06-07 12:02:47 +0500

    complete

---
+++ app.js
@@ -0,0 +1,18 @@
+//ASSIGNMENT 1
+
+//question 1
+alert("Greetings Visitor");
+
+//question 2
+alert("Error! Please enter a valid password.");
+
+//question 3
+alert("Welcome  to JS Land... \n Happy Coding!");
+
+//question 4
+alert("Welcome to JS Land...");
+alert("Prevenet this page from creating addtional dialogs.");
+
+//question 5
+console.log("Hello...I can run JS through my web browser's console");
+
