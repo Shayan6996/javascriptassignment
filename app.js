fa2dad83b9bebfc1b920b9fdeec94a30e2c88be0
Shayan6996 <65040094+Shayan6996@users.noreply.github.com>
2020-06-14 16:41:33 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -16,3 +16,127 @@ alert("Prevenet this page from creating addtional dialogs.");
 //question 5
 console.log("Hello...I can run JS through my web browser's console");
 
+
+//ASSIGNMENT 2
+
+//question 1
+var username;
+
+//question 2
+var myName;
+var myName = "Syed Shayan Ahmed";
+alert(myName);
+
+//question 3
+var message;
+var message = "Hello World";
+alert(message);
+
+//question 4
+var name = "Syed Shayan Ahmed",age = "22",skill = "Certified Mobile Application Development";
+alert(name);
+alert(age);
+alert(skill);   
+
+//question 5
+var a ="pizza"
+
+//question 6
+var email = "Fuhrer6996@gmail.com";
+alert("My" + " Email" + " Address" + " is " + email);
+
+//question 7
+var book = "A smart way to learn JavaScript";
+alert(book);
+
+// question 8
+document.write("Yah! I can write HTML content through JavaScipt");
+
+//Assignment 3
+
+
+//question 1
+var age = 22;
+alert("I " + " am " + age + " years " + "old");
+
+//question 2
+var a = "N";
+alert("You " + " have " + 'visited' + a + ' number' + 'of ' + 'times');
+
+//question 3
+var birthYear = 1998;
+alert('My' + 'birth' + 'year' +' is' + birthYear  + 'Data type of my declared varible is number' );
+ 
+//question 4
+
+var name = prompt("Visitor's name");
+var product = prompt("Your Product");
+var quantity = prompt("how much quantity");
+document.write(name + "ordered" + product + quantity + " on" + " XYZ" + "Clothing" + "store");
+
+//Assignment 4
+
+
+//question 1
+var name,age,gender;
+
+//question 2
+var name,age,gender,skill,hobby;
+
+
+//Assignment 5 
+
+
+//question 1
+var a = 5;
+var c = 5;
+var b = a+c;
+document.write(b);
+
+//question 2
+var a = 5;
+var c = 5;
+var b = a+c;
+var d = a*c;
+var x = a-c;
+var z = a/c;
+var q = a%c;
+document.write(b);
+document.write(d);
+document.write(x);
+document.write(z);
+document.write(q);
+
+//question 3
+var a;
+document.write("Value after variable declaration is: ??");
+var a = 10;
+document.write("Initial" + ' value ' + 'is ' + a);
+var a = a++;
+document.write("value" + ' afteer ' + 'increment ' + a);
+var c  = a+7;
+document.write("value" + ' afteer ' + 'addition ' + a);
+var a = a--;
+document.write("value" + ' afteer ' + 'decrement ' + a);
+var s = a%3;
+document.write("remainder is " + a);
+
+//question 4
+var t = 600;
+var c = t*600;
+document.write("Total cost to buy 5 tickets to a movie is " + c +" pkr");
+
+//question 5
+document.write("Table of 4");
+for ( var a = 1 ; a <= 10 ; a++)
+{
+    document.write("4" + "x" + a + "=" + 4*a +"<br>")
+}
+
+//question 6
+
+
+
+
+
+
